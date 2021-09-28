119dcddfd02b9d26b2e4ee9de3392f4e92fc20f4
ykikuchi <y.kikuchi5101@gmail.com>
2021-09-28 22:19:55 +0900

    revise1

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,5 @@ use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index']);
 Route::post('/todo/create', [TodoController::class, 'create']);
-Route::get('/todo/update', [TodoController::class, 'index']);
 Route::post('/todo/update', [TodoController::class, 'update']);
-Route::get('/todo/delete', [TodoController::class, 'index']);
 Route::post('/todo/delete', [TodoController::class, 'delete']);
